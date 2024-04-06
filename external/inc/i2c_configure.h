915715d8458c42d2fc8ad42accbb5ed82bd8d61b
Michal Plachta <mic.plachta@gmail.com>
2024-04-06 16:32:03 +0200

    Add deps to external folder. Final form.

---
+++ external/inc/i2c_configure.h
@@ -0,0 +1,6 @@
+#ifndef _I2C_CONFIGURE_H
+#define _I2C_CONFIGURE_H
+
+void i2c_configure(unsigned pclk);
+
+#endif
--- main.c
+++ main.c
@@ -11,6 +11,10 @@ void configureDMA();
 
 void configureUSART();
 
+void configureDiodes();
+
+void configureMouse();
+
 int main() {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN
                  | RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_DMA1EN;
@@ -22,51 +26,9 @@ int main() {
 
     configureUSART();
     configureDMA();
-
-    NVIC_EnableIRQ(DMA1_Stream6_IRQn);
-    USART2->CR1 |= USART_CR1_UE;
-
     i2c_enable();
-
     startAccelerometer();
-
-    // Link the TIM3 output to diodes.
-    GPIOafConfigure(GPIOA, 6, GPIO_OType_PP,
-                    GPIO_Low_Speed,
-                    GPIO_PuPd_NOPULL, GPIO_AF_TIM3);
-    GPIOafConfigure(GPIOA, 7, GPIO_OType_PP,
-                    GPIO_Low_Speed,
-                    GPIO_PuPd_NOPULL, GPIO_AF_TIM3);
-
-    TIM3->PSC = 99;
-    TIM3->ARR = 999;
-    TIM3->EGR = TIM_EGR_UG;
-    TIM3->CCR1 = 980;
-    TIM3->CCR2 = 980;
-
-    // Use PWM 2, output inverted.
-    TIM3->CCMR1 =
-        TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1 |
-        TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1PE |
-        TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1 |
-        TIM_CCMR1_OC2M_0 | TIM_CCMR1_OC2PE;
-    
-    TIM3->CCER = TIM_CCER_CC1E | TIM_CCER_CC1P |
-                 TIM_CCER_CC2E | TIM_CCER_CC2P;
-
-    TIM3->CR1 = TIM_CR1_ARPE |  TIM_CR1_CMS_0 |
-                TIM_CR1_CMS_1 | TIM_CR1_CEN;
-
-    // Fetch data from accelerometer every 20 ms.
-    TIM4->CR1 = TIM_CR1_URS;
-    TIM4->PSC = 15999;
-    TIM4->ARR = 19;
-    TIM4->EGR = TIM_EGR_UG;
-    TIM4->SR = ~TIM_SR_UIF;
-    TIM4->DIER =  TIM_DIER_UIE;
-    TIM4->CR1 |= TIM_CR1_CEN;
-
-    NVIC_EnableIRQ(TIM4_IRQn);
+    configureDiodes();
     
     while(1) {}
 }
@@ -81,6 +43,9 @@ void configureDMA() {
     DMA1_Stream6->PAR = (uint32_t)&USART2->DR;
 
     DMA1->HIFCR = DMA_HIFCR_CTCIF6;
+
+    NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+    USART2->CR1 |= USART_CR1_UE;
 }
 
 void configureUSART() {
@@ -101,6 +66,47 @@ void configureUSART() {
     USART2->CR2 = 0;
     USART2->BRR = (PCLK1_HZ + (BAUD / 2U)) / BAUD;
     USART2->CR3 = USART_CR3_DMAT;
+}
+
+void configureDiodes() {
+    TIM3->PSC = 99;
+    TIM3->ARR = 999;
+    TIM3->EGR = TIM_EGR_UG;
+    TIM3->CCR1 = 980;
+    TIM3->CCR2 = 980;
+
+    // Use PWM 2, output inverted.
+    TIM3->CCMR1 =
+        TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1 |
+        TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1PE |
+        TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1 |
+        TIM_CCMR1_OC2M_0 | TIM_CCMR1_OC2PE;
+    
+    TIM3->CCER = TIM_CCER_CC1E | TIM_CCER_CC1P |
+                 TIM_CCER_CC2E | TIM_CCER_CC2P;
+
+    TIM3->CR1 = TIM_CR1_ARPE |  TIM_CR1_CMS_0 |
+                TIM_CR1_CMS_1 | TIM_CR1_CEN;
+    
+    GPIOafConfigure(GPIOA, 6, GPIO_OType_PP,
+                    GPIO_Low_Speed,
+                    GPIO_PuPd_NOPULL, GPIO_AF_TIM3);
+    GPIOafConfigure(GPIOA, 7, GPIO_OType_PP,
+                    GPIO_Low_Speed,
+                    GPIO_PuPd_NOPULL, GPIO_AF_TIM3);
+}
+
+void configureMouse() {
+    // Fetch data from accelerometer every 20 ms.
+    TIM4->CR1 = TIM_CR1_URS;
+    TIM4->PSC = 15999;
+    TIM4->ARR = 19;
+    TIM4->EGR = TIM_EGR_UG;
+    TIM4->SR = ~TIM_SR_UIF;
+    TIM4->DIER =  TIM_DIER_UIE;
+    TIM4->CR1 |= TIM_CR1_CEN;
+
+    NVIC_EnableIRQ(TIM4_IRQn);
 }
 
 void TIM4_IRQHandler(void) {
